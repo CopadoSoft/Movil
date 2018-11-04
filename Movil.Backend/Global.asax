e66af8ea8ef070724545b5177aa4be861fb95650
Erik Copado P <erik_copado@hotmail.com>
2018-11-04 01:13:28 -0600

    Movil4Nov18

---
+++ Movil.Backend/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Movil.Backend.MvcApplication" Language="C#" %>
---
+++ Movil.Backend/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Movil.Backend.Startup))]
+namespace Movil.Backend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

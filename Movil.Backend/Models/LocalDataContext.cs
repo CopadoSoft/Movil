bcaa341881ac9e632b2eec3fd5ccd40c117742a5
Erik Copado P <erik_copado@hotmail.com>
2018-11-04 19:05:18 -0600

    Ejercicio04

---
+++ Movil.Backend/Models/LocalDataContext.cs
@@ -0,0 +1,8 @@
+﻿namespace Movil.Backend.Models
+{
+    using Domain.Models;
+    public class LocalDataContext: DataContext
+    {
+        public System.Data.Entity.DbSet<Movil.Common.Models.Product> Products { get; set; }
+    }
+}
